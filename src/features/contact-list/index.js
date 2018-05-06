af91867615d6ec83b82d8da1901feaacce0c5c74
Mark Tellez <mark.tellez@icloud.com>
2018-05-06 15:41:30 -0500

    basic paging

--- src/features/contact-list/index.js
+++ src/features/contact-list/index.js
@@ -4,7 +4,11 @@ import Contact from './contact'
 
 class ContactList extends React.Component {
   state = {
-    contacts: []
+    contacts: [],
+    per: 2,
+    page: 1,
+    totalPages: null,
+    hasMore: true,
   }
 
   componentWillMount() {
@@ -12,21 +16,32 @@ class ContactList extends React.Component {
   }
 
   loadContacts = () => {
-    fetch('https://student-example-api.herokuapp.com/v1/contacts.json')
+    const { per, page, contacts } = this.state
+    const url = `https://student-example-api.herokuapp.com/v1/contacts.json?per=${per}&page=${page}`
+    fetch(url)
       .then(response => response.json())
       .then(json => this.setState({
-        contacts: json.contacts
+        contacts: [...contacts, ...json.contacts]
       }))
   }
 
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page+1
+    }), this.loadContacts)
+  }
+
   render() {
-    return <ul className="contacts">
-      {
-        this.state.contacts.map(contact => <li key={contact.id}>
-          <Contact {...contact} />
-        </li>)
-      }
-    </ul>
+    return <div>
+      <ul className="contacts">
+        {
+          this.state.contacts.map(contact => <li key={contact.id}>
+            <Contact {...contact} />
+          </li>)
+        }
+      </ul>
+      <a onClick={this.loadMore}>Load more</a>
+    </div>
   }
 }
 
